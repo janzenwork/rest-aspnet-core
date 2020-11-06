5a39d2f6bbdb06cf13134a2525cd0e7309ff1c33
janzen-aeva <jhouchen-wilder@aevapharmacy.com>
2020-11-06 14:49:35 -0800

    Add project files.

---
+++ Controllers/crudController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rest_aspnet_core.Controllers
+{
+    public class crudController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
